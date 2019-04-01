a535c096ef1213fc054e48fa78c7d9cfa95377c8
Evgeny Mironov <mironov-hse@yandex.ru>
2019-04-01 17:08:11 +0200

    implemented FAB with speed dial
    added a custom toolbar at the top
    settings, night mode, share, about - w-i-p

--- app/src/main/java/com/example/jtodolister/MainActivity.java
+++ app/src/main/java/com/example/jtodolister/MainActivity.java
@@ -2,18 +2,30 @@ package com.example.jtodolister;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab,fab_add,fab_long,fab_pic,fab_loc,fab_time;
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+    public Boolean isNightModeChecked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,34 +33,131 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ArrayList<String> items = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            items.add("This is #" + i + " item of the list.");
-        }
+        Toolbar main_toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(main_toolbar);
+        //TODO: landscape icons for settings
 
-
-        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list_view);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,1));
-        recyclerView.setAdapter(new ListAdapter(this, items));
-
-        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int temp = items.size();
-                items.add("New item #" + temp + "! Hurray!");
-                if (items.size() > 2) {
-                    recyclerView.getAdapter().notifyItemInserted(items.size()-1);
-                } else {
-                    recyclerView.getAdapter().notifyDataSetChanged();
-                }
-
-            }
-        });
+        getNiceFloatingActionButton();
 
     }
 
-    private static class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.fab:
+
+                animateFAB();
+                break;
+            case R.id.fab_action_add:
+
+                Log.d("XXX", "Fab add");
+                break;
+            case R.id.fab_action_add_long:
+
+                Log.d("XXX", "Fab long");
+                break;
+            case R.id.fab_action_add_loc:
+
+                Log.d("XXX", "Fab loc");
+                break;
+            case R.id.fab_action_add_pic:
+
+                Log.d("XXX", "Fab pic");
+                break;
+            case R.id.fab_action_add_time:
+
+                Log.d("XXX", "Fab time");
+                break;
+        }
+    }
+
+    public void goToSettings(MenuItem item){
+        //TODO: intent to settings page
+        Log.d("XXX","Going to settings");
+    }
+
+    public void goToAbout(MenuItem item) {
+        //TODO: intent to about page
+        Log.d("XXX","Going to about");
+    }
+
+    public void toggleNightMode(MenuItem item){
+        //TODO: Night mode
+        if (isNightModeChecked) {
+            item.setChecked(false);
+            isNightModeChecked = false;
+            Log.d("XXX","turning night mode off");
+        } else {
+            item.setChecked(true);
+            isNightModeChecked = true;
+            Log.d("XXX","turning night mode on");
+        }
+    }
+    //----------------------FAB STUFF------------------------------
+    //-------------------------------------------------------------
+    private void getNiceFloatingActionButton() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab_add = (FloatingActionButton) findViewById(R.id.fab_action_add);
+        fab_long = (FloatingActionButton) findViewById(R.id.fab_action_add_long);
+        fab_loc = (FloatingActionButton) findViewById(R.id.fab_action_add_loc);
+        fab_pic = (FloatingActionButton) findViewById(R.id.fab_action_add_pic);
+        fab_time = (FloatingActionButton) findViewById(R.id.fab_action_add_time);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        fab.setOnClickListener(this);
+        fab_add.setOnClickListener(this);
+        fab_long.setOnClickListener(this);
+        fab_loc.setOnClickListener(this);
+        fab_pic.setOnClickListener(this);
+        fab_time.setOnClickListener(this);
+    }
+
+    public void animateFAB(){
+        if(isFabOpen){
+            fab.startAnimation(rotate_backward);
+            fab_add.startAnimation(fab_close);
+            fab_long.startAnimation(fab_close);
+            fab_loc.startAnimation(fab_close);
+            fab_pic.startAnimation(fab_close);
+            fab_time.startAnimation(fab_close);
+            fab_add.setClickable(false);
+            fab_long.setClickable(false);
+            fab_loc.setClickable(false);
+            fab_pic.setClickable(false);
+            fab_time.setClickable(false);
+            isFabOpen = false;
+            Log.d("XXX", "close");
+        } else {
+            fab.startAnimation(rotate_forward);
+            fab_add.startAnimation(fab_open);
+            fab_long.startAnimation(fab_open);
+            fab_loc.startAnimation(fab_open);
+            fab_pic.startAnimation(fab_open);
+            fab_time.startAnimation(fab_open);
+            fab_add.setClickable(true);
+            fab_long.setClickable(true);
+            fab_loc.setClickable(true);
+            fab_pic.setClickable(true);
+            fab_time.setClickable(true);
+            isFabOpen = true;
+            Log.d("XXX","open");
+        }
+    }
+
+    //-------------------------------------------------------------
+    //----------------------FAB STUFF------------------------------
+
+    private static class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         private final LayoutInflater myInflater;
         private final ArrayList myData;
 
@@ -56,7 +165,6 @@ public class MainActivity extends AppCompatActivity {
             myInflater = LayoutInflater.from(context);
             myData = data;
         }
-
 
         @NonNull
         @Override
