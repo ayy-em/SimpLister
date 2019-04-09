9dde51e3e0989657c9c9950cb86ff6021fd85d64
Evgeny Mironov <mironov-hse@yandex.ru>
2019-04-09 15:10:14 +0200

    added a splash screen, changed the theme, hid api key

--- app/src/main/java/com/example/jtodolister/MainActivity.java
+++ app/src/main/java/com/example/jtodolister/MainActivity.java
@@ -55,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
